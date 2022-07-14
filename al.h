5f022cd02f585f19d05419dd2f2d568de46be030
banahaker <pn0818x@outlook.com>
2022-07-14 19:43:17 +0800

    feat: new data-structure: linked list

--- al.h
+++ al.h
@@ -6,8 +6,8 @@
 	output: none
 */
 #define println(text) \
-		printf(text); \
-		printf("\n");
+	printf(text);       \
+	printf("\n");
 
 /* 	display a array
 	input{
@@ -17,30 +17,40 @@
 	}
 	output: arr[]
 */
-void arrayDisplay(int arr[], int length, int direction) {
-	if (direction == 0) {
+void arrayDisplay(int arr[], int length, int direction)
+{
+	if (direction == 0)
+	{
 		printf("[");
-		for (int i = 0; i < length; i++) {
-			if (i != length - 1) {
+		for (int i = 0; i < length; i++)
+		{
+			if (i != length - 1)
+			{
 				printf(" %d,", arr[i]);
 			}
-			else {
+			else
+			{
 				printf(" %d ]\n", arr[i]);
 			}
 		}
 	}
-	else if (direction == 1) {
+	else if (direction == 1)
+	{
 		printf("[");
-		for (int i = length - 1; i >= 0; i--) {
-			if (i > 0) {
+		for (int i = length - 1; i >= 0; i--)
+		{
+			if (i > 0)
+			{
 				printf(" %d,", arr[i]);
 			}
-			else {
+			else
+			{
 				printf(" %d ]\n", arr[0]);
 			}
 		}
 	}
-	else {
+	else
+	{
 		printf("direction error");
 		return;
 	}
@@ -63,10 +73,12 @@ void arrayDisplay(int arr[], int length, int direction) {
 	input(arr: array)
 	output: maxNum: int
 */
-int maxInArray(int* arr, int length){
+int maxInArray(int *arr, int length)
+{
 	int current;
 	int maxNum = arr[0];
-	for(int i=1; i<length; i++){
+	for (int i = 1; i < length; i++)
+	{
 		current = arr[i];
 		maxNum = MAX(maxNum, current);
 	}
@@ -77,10 +89,12 @@ int maxInArray(int* arr, int length){
 	input(arr: array)
 	output: minNum: int
 */
-int minInArray(int* arr, int length){
+int minInArray(int *arr, int length)
+{
 	int current;
 	int minNum = arr[0];
-	for(int i=1; i<length; i++){
+	for (int i = 1; i < length; i++)
+	{
 		current = arr[i];
 		minNum = MIN(minNum, current);
 	}
@@ -89,14 +103,18 @@ int minInArray(int* arr, int length){
 
 // Sort
 /* Bubble Sort
-   input(arr: array, length: int)
-   output: none
+	 input(arr: array, length: int)
+	 output: none
 */
-void bubbleSort(int arr[], int length) {
+void bubbleSort(int arr[], int length)
+{
 	int tmp;
-	for (int t = 0; t < length - 1; t++) {
-		for (int i = 0; i < length - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
+	for (int t = 0; t < length - 1; t++)
+	{
+		for (int i = 0; i < length - 1; i++)
+		{
+			if (arr[i] > arr[i + 1])
+			{
 				tmp = arr[i];
 				arr[i] = arr[i + 1];
 				arr[i + 1] = tmp;
@@ -109,12 +127,15 @@ void bubbleSort(int arr[], int length) {
 	input(arr: array, length: int)
 	output: none
 */
-void insertSort(int arr[], int length) {
+void insertSort(int arr[], int length)
+{
 	int key, j;
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++)
+	{
 		key = arr[i];
 		j = i - 1;
-		while (j >= 0 && arr[j] > key) {
+		while (j >= 0 && arr[j] > key)
+		{
 			arr[j + 1] = arr[j];
 			j--;
 		}
@@ -128,15 +149,20 @@ void insertSort(int arr[], int length) {
 	output: target's index(if target is not in arr will return -1): int
 	notion: must use sorted array
 */
-int binarySearch(int* arr, int length, int target) {
+int binarySearch(int *arr, int length, int target)
+{
 	int mid;
 	int left = 0;
 	int right = length - 1;
-	while (left <= right) {
-		mid = (left+right)/2;
-		if (arr[mid] == target) return mid;
-		else if (arr[mid] < target) left = mid + 1;
-		else if (arr[mid] > target) right = mid - 1;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if (arr[mid] == target)
+			return mid;
+		else if (arr[mid] < target)
+			left = mid + 1;
+		else if (arr[mid] > target)
+			right = mid - 1;
 	}
 	return -1;
 }
@@ -145,9 +171,12 @@ int binarySearch(int* arr, int length, int target) {
 	input(arr: array, length: int, target: int)
 	output: target's index(if target isn't in arr will return -1): int
 */
-int linearSearch(int* arr, int length, int target) {
-	for(int i=0; i<length; i++){
-		if(arr[i] == target){
+int linearSearch(int *arr, int length, int target)
+{
+	for (int i = 0; i < length; i++)
+	{
+		if (arr[i] == target)
+		{
 			return i;
 		}
 	}
@@ -159,21 +188,41 @@ int linearSearch(int* arr, int length, int target) {
 	input(location: int, arr: array, length: int, value: int)
 	output: none(inserted array)
 */
-int insertElement(int location, int arr[], int length, int value){
-    if (location >= length ) return 0;
-    for(int i=length-2; i>=location; i--)
-        arr[i + 1] = arr[i];
-    arr[location] = value;
+int insertElement(int location, int arr[], int length, int value)
+{
+	if (location >= length)
+		return 0;
+	for (int i = length - 2; i >= location; i--)
+		arr[i + 1] = arr[i];
+	arr[location] = value;
 }
 
 /*	delete element
 	input(location: int, arr: array, length: int)
 	output: none(deleted array)
 */
-int deleteElment(int location, int arr[], int length) {
-    if(location >= length) return 0;
-    for(int i=location; i<length; i++){
-        arr[i] = arr[i+1];
-    }
-    arr[length-1] = 0;
+int deleteElement(int location, int arr[], int length)
+{
+	if (location >= length)
+		return 0;
+	for (int i = location; i < length; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
+	arr[length - 1] = 0;
+}
+
+/* Basic Data structure */
+typedef struct
+{
+	int data;
+	struct Node *next;
+} Node;
+
+Node *new_node(Node *previous, int data)
+{
+	Node *new_node = (struct Node *)malloc(sizeof(Node));
+	new_node->data = data;
+	previous->next = new_node;
+	return new_node;
 }
--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "al.h" // 引入函式庫 import library
 
-int main() {
+int main()
+{
 	println("algorithm library test"); // 全行顯示 (自動加入'\n')
 	int arr[10] = {5, 4, 3, 8, 7, 6, 1, 9, 288, 2};
-	arrayDisplay(arr, 10, 0); // 顯示陣列 display array
-	insertSort(arr, 10); // 陣列排序 array sort
-	arrayDisplay(arr, 10, 0); // 顯示陣列 display array
+	arrayDisplay(arr, 10, 0);										 // 顯示陣列 display array
+	insertSort(arr, 10);												 // 陣列排序 array sort
+	arrayDisplay(arr, 10, 0);										 // 顯示陣列 display array
 	int searchResult = binarySearch(arr, 10, 4); // 陣列搜尋 array search
 	printf("searchResult: %d\n", searchResult);
 	int maxValueInArray = maxInArray(arr, 10); // 陣列最大值 largest value in array
@@ -17,7 +18,7 @@ int main() {
 	printf("max: %f, min: %f\n", max, min);
 	insertElement(2, arr, 10, 13); // 在 arr 中的索引三插入13 insert 13 to index_2 in arr
 	arrayDisplay(arr, 10, 0);
-	deleteElment(3, arr, 10); // 刪除 arr 中的索引 3 delete index_3 in arr
+	deleteElement(3, arr, 10); // 刪除 arr 中的索引 3 delete index_3 in arr
 	arrayDisplay(arr, 10, 0);
 	return 0;
 }
